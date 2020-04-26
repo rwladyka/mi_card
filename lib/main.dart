52f7e54bb0e4f987ca43980d5c869596333d3a30
Rodrigo Wladyka <rodrigo.wladyka@bluefoot.com.br>
2020-04-26 02:32:43 -0300

    Finished module

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/rw.jpeg'),
+              ),
+              Text(
+                'Rodrigo Wladyka',
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                  fontFamily: 'Black Ops One'
+                ),
+              ),
+              Text(
+                'SOFTWARE ENGENEER',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontFamily: 'Source Sans Pro',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text('+55 41 99710-3048',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20
+                    ),
+                )
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text('rodrigo.wladyka@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20
+                      ),)
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
